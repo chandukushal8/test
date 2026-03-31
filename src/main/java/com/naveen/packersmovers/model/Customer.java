2740bf2fd750aa3185c194393ae2793ef4e8d893
chandukushal8 <chandukushal8@gmail.com>
2026-04-01 00:01:25 +0530

    Update Customer.java

--- src/main/java/com/naveen/packersmovers/model/Customer.java
+++ src/main/java/com/naveen/packersmovers/model/Customer.java
@@ -1,92 +1,19 @@
-package com.naveen.packersmovers.model;
+package com.chandu.userform.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
+public class User {
 
-public class Customer {
     @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
-    private int id;
-    private String phone;
-    private String fullName;
-    private String fromAddress;
-    private String toAddress;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+    private String place;
+    private String contact;
     private String email;
-    private String estDate;
-    private String moveSize;
-
-	/*
-	 * public User() {
-	 * 
-	 * }
-	 */
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getfullName() {
-        return fullName;
-    }
-
-    public void setfullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getfromAddress() {
-        return fromAddress;
-    }
-
-    public void setfromAddress(String fromAddress) {
-        this.fromAddress = fromAddress;
-    }
-
-    public String gettoAddress() {
-        return toAddress;
-    }
-
-    public void settoAddress(String toAddress) {
-        this.toAddress = toAddress;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getestDate() {
-        return estDate;
-    }
-
-    public void setestDate(String estDate) {
-        this.estDate = estDate;
-    }
-
-    public String getmoveSize() {
-        return moveSize ;
-    }
-
-    public void setmoveSize(String moveSize) {
-        this.moveSize = moveSize;
-    }
 }
